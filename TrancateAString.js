1931b38a16990bebb32864f1d0b9c535fb89645d
Ann <anna.muzykina83@gmail.com>
2019-04-12 19:25:51 +0200

    Create TrancateAString.js

---
+++ TrancateAString.js
@@ -0,0 +1,15 @@
+/*
+Truncate a String
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+We need to reduce the length of the string or truncate it if it is longer than the given maximum lengths specified and add ... to the end.
+If it is not that long then we keep it as is.
+
+Relevant Links
+String.prototype.slice()
+
+*/
+
