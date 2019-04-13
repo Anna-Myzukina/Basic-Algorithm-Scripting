db028f99d1fa7f163635120eedd1ebdef6e1db0d
Ann <anna.muzykina83@gmail.com>
2019-04-13 22:18:55 +0200

    Update TrancateAString.js

--- TrancateAString.js
+++ TrancateAString.js
@@ -11,5 +11,8 @@ If it is not that long then we keep it as is.
 Relevant Links
 String.prototype.slice()
 
+
+//We need to reduce the length of the string or truncate it if it is longer than
+the given maximum lengths specified and add ... to the end. If it is not that long then we keep it as is.
 */
 
