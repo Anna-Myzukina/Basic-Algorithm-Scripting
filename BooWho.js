9dfa3e3a40b2fc5df93dd8eccaa4b1a6d4fab3bc
Ann <anna.muzykina83@gmail.com>
2019-04-14 14:57:56 +0200

    Update BooWho.js

--- BooWho.js
+++ BooWho.js
@@ -6,8 +6,9 @@ Boolean primitives are true and false.
 
 Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 */
-function booWho(bool){
-return typeof bool === "boolean";
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return typeof bool === 'boolean';
 }
 
 booWho(null);
